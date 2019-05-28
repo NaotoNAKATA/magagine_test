11b572c85d1afd94be84e5f1dfde0188381b8550
NaotoNAKATA <nakatanaoto55@gmail.com>
2019-05-28 13:42:45 +0900

    日経ソフトウェア2019年1月号 機械学習
    バージョン確認

---
+++ titanic/part1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+import pandas
+import numpy
+import matplotlib
+import sklearn
+
+if __name__ == '__main__':
+	print(pandas.__name__, pandas.__version__)
+	print(numpy.__name__, numpy.__version__)
+	print(matplotlib.__name__, matplotlib.__version__)
+	print(sklearn.__name__, sklearn.__version__)
+
+
